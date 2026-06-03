7e27bdf65025700f2176774da6d5fd135cc7be28
baccataco <kailynle@gmail.com>
2026-06-03 14:29:34 -0700

    upload

--- asgn5/src/asgn5.js
+++ asgn5/src/asgn5.js
@@ -290,7 +290,7 @@ function createPlaceholderDuck() {
 // --- CUSTOM 3D MODEL ---
 function loadDuckModel() {
     const loader = new FBXLoader();
-    const url = './Psyduck.fbx'; 
+    const url = './src/Psyduck.fbx'; 
 
     loader.load(url, 
         (object) => {
